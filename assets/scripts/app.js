204daa97e6c481693156ba483da7dd9379642966
Fernando Coelho <nandoacoelho@gmail.com>
2016-01-17 15:54:01 -0200

    initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,44 @@
+(function() {
+	var app = angular.module('gemStore', []);
+
+	app.controller('ReviewController', function() {
+		this.review = {};
+		this.addReview = function(product){
+			this.review.createdOn = Date.now();
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
+	app.controller('StoreController', ['$http', function($http) {
+		var store = this;
+		store.products = [];
+		$http.get('file:///Users/fernandocoelho/Downloads/angular-code-school/products.json').success(function(data) {
+			store.products = data;
+		});
+	}]);
+
+	app.directive('storeProducts', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'store-products.html',
+		};
+	});
+	
+	app.directive('panelSection', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'panel-section.html',
+			controller: function(){
+				this.tab = 1;
+				this.selectTab = function(setTab) {
+					this.tab = setTab;
+				};
+				this.isSelected = function(checkTab) {
+					return this.tab === checkTab;
+				}
+			},
+			controllerAs: 'panel'
+		};
+	});
+})();
