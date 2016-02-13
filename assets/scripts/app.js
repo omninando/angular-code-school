3827e7768c041cbf2dd20af32fda457d585f3eb0
Fernando Coelho <nandoacoelho@gmail.com>
2016-02-13 17:24:06 -0200

    Update JSON url

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,7 +13,7 @@
 	app.controller('StoreController', ['$http', function($http) {
 		var store = this;
 		store.products = [];
-		$http.get('/angular-code-school/products.json').success(function(data) {
+		$http.get('products.json').success(function(data) {
 			store.products = data;
 		});
 	}]);
